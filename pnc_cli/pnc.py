846e6de11920ea8b396d2682ead9cb31372361b7
Honza Brázdil <jbrazdil@redhat.com>
2018-03-22 17:28:39 +0100

    [NCL-3608] argument spoofing

--- pnc_cli/pnc.py
+++ pnc_cli/pnc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # PYTHON_ARGCOMPLETE_OK
 import argh
+import sys
 
 from pnc_cli import bpmbuildconfigurations
 from pnc_cli import buildconfigsetrecords
@@ -141,8 +142,27 @@ parser.add_commands([brewpush.push_build,
                      namespace="brew-push", namespace_kwargs=brewpush.namespace_kwargs)
 parser.autocomplete()
 
+replace_map = { 'create_product': ['product', 'create'],
+                'get_product': ['product', 'get']}
+
+def spoof_argv():
+    i=1
+    while i < len(sys.argv) and sys.argv[i][0] == '-':
+        i += 1
+    if i < len(sys.argv):
+        cmd = sys.argv[i]
+        if cmd in replace_map:
+            sys.argv[i] = replace_map[cmd][0]
+            sys.argv.insert(i+1, replace_map[cmd][1])
+
 
 def main():
+    spoof_argv()
+    print "0: " + sys.argv[0]
+    print "1: " + sys.argv[1]
+    print "2: " + sys.argv[2]
+    print "3: " + sys.argv[3]
+    print "4: " + sys.argv[4]
     parser.dispatch()
 
 
